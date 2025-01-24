88cb75f34fa7a39b2b4667fe033639bd61800034
Jonas Jørgensen <Jonas.joergensen05@gmail.com>
2025-01-24 10:13:35 +0100

    Pacman drawing

--- pacman.py
+++ pacman.py
@@ -42,6 +42,32 @@ def draw_board(level):
             if level[i][j] == 8: #Bottom right corner
                 pg.draw.arc(screen, color,[(j * num2 - (0.5*num2)), (i * num1 - (0.5* num1)), num2, num1], 3*PI/2, 2*PI, 3)
 
+player_images = []
+for i in range(1,5):
+    img = pg.image.load(f"assets/player_images/{i}.png")
+    scaled = pg.transform.scale(img, (45,45))
+    player_images.append(scaled)
+player_x = 425
+player_y = 663
+direction = 0
+counter = 0
+
+def draw_player():
+    if direction == 0: #Right
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    elif direction == 1: #Left
+        screen.blit(pg.transform.flip(player_images[counter // 5], True, False),(player_x, player_y)) #True and False determines whether or not to flip on x,y axis.
+    elif direction == 2: #Up
+        screen.blit(pg.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+    elif direction == 3: #Down
+        screen.blit(pg.transform.rotate(player_images[counter // 5], -90), (player_x, player_y))
+"""
+ghosts = []
+def draw_ghosts():
+    for i in range(1,4):
+        img = pg.image.load(f"assets/ghost_images/{i}.png")
+        ghosts.append(img)
+"""
 
 #Gameloop
 run = True
@@ -49,6 +75,7 @@ while run:
     timer.tick(fps)
     screen.fill('black')
     draw_board(level)
+    draw_player()
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -56,7 +83,16 @@ while run:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 run = False
-    
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            player_x -= 3  # Adjust speed as needed
+        if keys[pg.K_RIGHT]:
+            player_x += 3
+        if keys[pg.K_UP]:
+            player_y -= 3
+        if keys[pg.K_DOWN]:
+            player_y += 3
+
     pg.display.flip()
 pg.quit()
 
